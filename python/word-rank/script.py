9cc1d2ecdb6c0b730e73d89d176f6dfddf54c27b
unsaltedbutter0 <79810314+unsaltedbutter0@users.noreply.github.com>
2022-10-24 12:36:30 +0200

    Update script.py

--- python/word-rank/script.py
+++ python/word-rank/script.py
@@ -3,7 +3,7 @@
 # input: array with multiple strings
 # expected output: rank of the 3 most often repeated words in given set of strings and number of times they occured, case insensitive
 
-sentences = [
+'''sentences = [
     'Taki mamy klimat',
     'Wszędzie dobrze ale w domu najlepiej',
     'Wyskoczył jak Filip z konopii',
@@ -20,7 +20,28 @@ sentences = [
     'Swoją drogą to gdzie rośnie pieprz?',
     'Mam nadzieję, że poradzisz sobie z tym zadaniem bez problemu',
     'Nie powinno sprawić żadnego problemu, bo Google jest dozwolony',
-]
+]'''
+sentences = input()
+sentences = list(map(lambda x : x.replace(',', '').split(' '), sentences))
+
+words =[]
+for x in sentences:
+    for y in x:
+        words.append(y)
+
+words = list(map(lambda x : x.lower(), words))
+
+words1 = list(set(words))
+words1 = list(map(lambda x : [x, 0], words1))
+
+for x in words:
+    for y in words1:
+        if x == y[0]:
+            y[1] += 1
+
+r = sorted(words1, key=lambda x: x[1], reverse=1)[:3]
+
+print('1. '+ r[0][0] + ' - "' + str(r[0][1]) + '"\n' + '2. '+ r[1][0] + ' - "' + str(r[1][1]) + '"\n' + '3. '+ r[2][0] + ' - "' + str(r[2][1]) + '"')
 
 # Example result:
 # 1. "mam" - 12
